27d5ee2cca4d798ac4b78bc32e9acb58b524f028
DESKTOP-UKDCDPL\usuario <warjav.work@gmail.com>
2021-07-30 13:11:01 +0200

    Primeros parámetros

--- TSEngine/app.ts
+++ TSEngine/app.ts
@@ -1,16 +1,26 @@
 ﻿namespace TSE {
 
     export class Engine {
+        private _count: number = 0;
         public constructor() {
             console.log("Hello World")
         }
 
         public start(): void {
+            this.loop();
+        }
+        private loop(): void {
+            this._count++;
+
+            document.title = this._count.toString();
+
+            requestAnimationFrame(this.loop.bind(this));
 
         }
     }
 }
 window.onload = function () {
     let engine = new TSE.Engine();
+    engine.start();
     document.body.innerHTML += "Foo";
 }
--- TSEngine/main.js
+++ TSEngine/main.js
@@ -2,9 +2,16 @@ var TSE;
 (function (TSE) {
     var Engine = /** @class */ (function () {
         function Engine() {
+            this._count = 0;
             console.log("Hello World");
         }
         Engine.prototype.start = function () {
+            this.loop();
+        };
+        Engine.prototype.loop = function () {
+            this._count++;
+            document.title = this._count.toString();
+            requestAnimationFrame(this.loop.bind(this));
         };
         return Engine;
     }());
@@ -12,6 +19,7 @@ var TSE;
 })(TSE || (TSE = {}));
 window.onload = function () {
     var engine = new TSE.Engine();
+    engine.start();
     document.body.innerHTML += "Foo";
 };
 //# sourceMappingURL=main.js.map
